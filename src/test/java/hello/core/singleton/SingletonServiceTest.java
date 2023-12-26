0ab5d1812d4e102a121fc61f3320577dced0c3b4
stelse99 <stelse99@gmail.com>
2023-12-26 20:59:06 +0900

    bean definition

---
+++ src/test/java/hello/core/singleton/SingletonServiceTest.java
@@ -0,0 +1,58 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SingletonServiceTest {
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    public void singletonServiceTest() {
+        //private으로 생성자를 막아두었다. 컴파일 오류가 발생한다. //new SingletonService();
+        //1. 조회: 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService1 = SingletonService.getInstance();
+        //2. 조회: 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService2 = SingletonService.getInstance();
+        //참조값이 같은 것을 확인
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+        // singletonService1 == singletonService2
+        assertThat(singletonService1).isSameAs(singletonService2);
+        singletonService1.logic();
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new
+                AnnotationConfigApplicationContext(AppConfig.class);
+        //1. 조회: 호출할 때 마다 같은 객체를 반환
+        MemberService memberService1 = ac.getBean("memberService",
+                MemberService.class);
+        //2. 조회: 호출할 때 마다 같은 객체를 반환
+        MemberService memberService2 = ac.getBean("memberService",
+                MemberService.class);
+        //참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+        //memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new
+                AnnotationConfigApplicationContext(AppConfig.class);
+        //AppConfig도 스프링 빈으로 등록된다.
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        //출력: bean = class hello.core.AppConfig$$SpringCGLIB$$0
+    }
+}
